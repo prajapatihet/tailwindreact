a29769e183c5443a9fe1aa9ab690d33a6301a8e9
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-02 21:11:57 +0530

    useState working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import weblogo from './images/logos.svg';
 
 function App() {
 
+  let [count, newCount] = useState(1)
   let displayData = () => {
-    alert("Welcome to page")
+    newCount(count + 1)
+    // alert("Welcome to page")
   }
   let addData = (a, b) => {
     console.log(a + b);
@@ -15,7 +18,8 @@ function App() {
     <div className="App">
       <img width={200} src={weblogo} alt='weblogo' />
       <button className='bg-[red] p-[10px] mr-4' onClick={() => addData(20, 25)}>Add Data</button>
-      <button className='bg-[red] p-[10px]' onClick={displayData}>Save</button>
+      <button className='bg-[red] p-[10px] mr-4' onClick={displayData}>Save</button>
+      {count}
       <Header />
       <h1 className='text-[40px] text-red-600 font-bold'>Welcome to Page</h1>
     </div>
