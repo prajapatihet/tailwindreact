cba4cd69807b9289b846ecdab8f1bc4acba0d006
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-02 20:49:41 +0530

    param event handle

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ function App() {
   let displayData = () => {
     alert("Welcome to page")
   }
+  let addData = (a, b) => {
+    console.log(a + b);
+  }
+
   return (
     <div className="App">
       <img width={200} src={weblogo} alt='weblogo' />
+      <button className='bg-[red] p-[10px] mr-4' onClick={() => addData(20, 25)}>Add Data</button>
       <button className='bg-[red] p-[10px]' onClick={displayData}>Save</button>
       <Header />
       <h1 className='text-[40px] text-red-600 font-bold'>Welcome to Page</h1>
