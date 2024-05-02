32eeb0b6c0167957900feb29fd5728d5f74eb835
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-02 20:44:08 +0530

    normal event handle

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import Header from './Header';
 import weblogo from './images/logos.svg';
 
 function App() {
+
+  let displayData = () => {
+    alert("Welcome to page")
+  }
   return (
     <div className="App">
       <img width={200} src={weblogo} alt='weblogo' />
+      <button className='bg-[red] p-[10px]' onClick={displayData}>Save</button>
       <Header />
       <h1 className='text-[40px] text-red-600 font-bold'>Welcome to Page</h1>
     </div>
