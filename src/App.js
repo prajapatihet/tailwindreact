095bca9cc366c60fb009ba0905baab181423e5aa
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-02 20:32:52 +0530

    import img and ext css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './Header';
+import weblogo from './images/logos.svg';
 
 function App() {
   return (
     <div className="App">
+      <img width={200} src={weblogo} alt='weblogo' />
+      <Header />
       <h1 className='text-[40px] text-red-600 font-bold'>Welcome to Page</h1>
     </div>
   );
