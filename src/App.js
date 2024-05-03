2de18d477e4c17e1bff090bd51ca621292c93c85
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 11:34:21 +0530

    conditional on components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import weblogo from './images/logos.svg';
 
 function App() {
 
-  let [count, newCount] = useState(1)
+  let [count, newCount] = useState(1);
+  let [pshow, newShow] = useState(true);
   let displayData = () => {
     newCount(count + 1)
     // alert("Welcome to page")
   }
+  let temp = '';
+  if (pshow) {
+    temp = <Card />
+    // <>
+    //   <p>Welcome to App</p>
+    //   <button className='bg-[red] p-4' onClick={() => newShow(!pshow)}>Hide</button>
+    // </>
+  } else {
+    temp = ''
+    // <button className='bg-[red] p-4' onClick={() => newShow(!pshow)}>Show</button>
+  }
+
   let addData = (a, b) => {
     console.log(a + b);
   }
 
   return (
     <div className="App">
+      {temp}
       <img width={200} src={weblogo} alt='weblogo' />
       <button className='bg-[red] p-[10px] mr-4' onClick={() => addData(20, 25)}>Add Data</button>
       <button className='bg-[red] p-[10px] mr-4' onClick={displayData}>Save</button>
@@ -27,3 +41,10 @@ function App() {
 }
 
 export default App;
+
+
+let Card = () => {
+  return (
+    <h1>Welcome to page</h1>
+  )
+}
